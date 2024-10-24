0da77a0bba11bb785bd4a197f608f521389e4ac2
Gayeon Lee <gayean430@gmail.com>
2024-10-25 00:53:22 +0900

    fix: resolve conflicts

--- src/components/login/KakaoLogin.jsx
+++ src/components/login/KakaoLogin.jsx
@@ -3,34 +3,6 @@ import { Button } from "../ui/button";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-<<<<<<< HEAD
-const KakaoLogin = () => {
-  const kakaoClientId = import.meta.env.VITE_KAKAO_CLIENT_ID;
-  const kakaoRedirectURL = "http://localhost:5173/";
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}&redirect_uri=${kakaoRedirectURL}&response_type=code&prompt=login`;
-
-  const loginURL = () => {
-    const width = 450;
-    const height = 600;
-    const left = window.screen.width / 2 - width / 2;
-    const top = window.screen.height / 2 - height / 2;
-
-    window.open(
-      KAKAO_AUTH_URL,
-      "KakaoLogin",
-      `width=${width},height=${height},left=${left},top=${top}`
-    );
-  };
-
-  return (
-    <div className="h-14 w-full">
-      <Button variant="kakao" onClick={loginURL}>
-        <img className="h-2/6" src={kakao} alt="kakao_icon" />
-        <span>카카오 로그인</span>
-      </Button>
-    </div>
-  );
-=======
 const clientId = import.meta.env.VITE_KAKAO_CLIENT_ID;
 // const redirectURI = "http://localhost:5173/member/oauth/kakao";
 const redirectURI = "https://www.newsfit.shop/member/oauth/kakao";
@@ -38,7 +10,6 @@ const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${clie
 
 export const handleKakaoClick = () => {
   window.location.href = KAKAO_AUTH_URL;
->>>>>>> 5774009 (feature: implement Kakao OAuth login flow)
 };
 
 const authKakaoLogin = async (code) => {
