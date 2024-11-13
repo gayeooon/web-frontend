14a828ee4df957647ec45242a1c135594507fc1c
Gayeon Lee <gayean430@gmail.com>
2024-11-13 22:29:49 +0900

    feature: update OAuth redirect URIs as environment variables

--- src/components/login/NaverLogin.jsx
+++ src/components/login/NaverLogin.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
-const REDIRECT_URI = "http://localhost:5173/member/oauth/naver";
+const REDIRECT_URI = import.meta.env.VITE_NAVER_REDIRECT_URI;
 const STATE = "state";
 const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${NAVER_CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&state=${STATE}`;
 
