a50e9516ec1d6dac1f4a31bc790e4510050d804f
Gayeon Lee <gayean430@gmail.com>
2025-04-04 11:01:44 +0900

    Refactor: AuthProvider 컴포넌트 제거

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { AuthProvider } from '@/contexts/AuthProvider';
 import ScrollToTop from '@/components/ui/custom/ScrollToTop';
 import ToasterProvider from '@/contexts/ToasterProvider';
 import './globals.css';
@@ -15,12 +14,10 @@ const queryClient = new QueryClient({
 
 const App = ({ Component, pageProps }) => (
   <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <ToasterProvider>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ToasterProvider>
-    </AuthProvider>
+    <ToasterProvider>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ToasterProvider>
   </QueryClientProvider>
 );
 
